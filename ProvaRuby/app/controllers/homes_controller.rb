acb2f1769b8fcdc53f6c394289af57f2d978a254
Michael <capponi.1711759@studenti.uniroma1.it>
2018-07-24 22:17:48 +0200

    Aggiunte selezione e dettagli lega

--- ProvaRuby/app/controllers/homes_controller.rb
+++ ProvaRuby/app/controllers/homes_controller.rb
@@ -1,9 +1,15 @@
 class HomesController < ApplicationController
     before_action :authenticate_user!
-    def show
-        @prova = "prova"
+    def index
         @utente = current_user.email
+        @leghe = League.all
     end
+
+    def show
+		id = params[:id]
+		@lega = League.find(id)
+    end
+
     def new
         authorize! :read, Home, :message => "Non puoi accedere alla risorsa selezionata"
     end
--- ProvaRuby/config/routes.rb
+++ ProvaRuby/config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  
+
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    resource :home
-    root :to => redirect('/home')
-     get 'about'   => 'static_pages#about'
-     get 'contact' => 'static_pages#contact'
+    resources :homes
+    root :to => redirect('/homes')
+    get 'about'   => 'static_pages#about'
+    get 'contact' => 'static_pages#contact'
 end
