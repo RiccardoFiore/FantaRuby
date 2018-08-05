beb72f38c719cb07e00f9a9bd18ba16931f8ff03
Michael <capponi.1711759@studenti.uniroma1.it>
2018-08-05 16:54:59 +0200

    Cambiato budget iniziale

--- ProvaRuby/db/schema.rb
+++ ProvaRuby/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_05_135431) do
+ActiveRecord::Schema.define(version: 2018_08_05_145323) do
 
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -114,7 +114,7 @@ ActiveRecord::Schema.define(version: 2018_08_05_135431) do
     t.integer "league_id"
     t.string "favourite_team"
     t.string "username"
-    t.integer "budget", default: 500
+    t.integer "budget", default: 300
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
