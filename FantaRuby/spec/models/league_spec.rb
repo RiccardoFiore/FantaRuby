5f90891f759ce6258eda8822a0fb98f2b3dfd393
bucci1649197 <bucci.1649197@studenti.uniroma1.it>
2018-08-18 21:03:39 +0200

    test model league

---
+++ FantaRuby/spec/models/league_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+describe League, :type => :model do
+   
+  it "Create League" do
+league=League.new(:name => "legas", :players => 2, :description => "Descrizione", :status => "Aperta")
+    expect(league).to be_valid
+  end
+
+
+it "is not valid without a name" do
+        league=League.new(:name => nil)
+        expect(league).to_not be_valid
+    end
+
+it "is not valid without a players" do
+        league=League.new(:name => "lega", :players => nil)
+        expect(league).to_not be_valid
+    end
+
+ it "is not valid without a description" do
+        league=League.new(:name => "lega", :players => 2, :description => nil)
+        expect(league).to_not be_valid
+    end
+ it "is not valid without a status" do
+        league=League.new(:name => "lega", :players => 2, :description => "descrizione", :status => nil)
+        expect(league).to_not be_valid
+    end
+
+end
