36406ad1ad950ae93dc7a8b6cb8a6a3e61c74032
Michael <capponi.1711759@studenti.uniroma1.it>
2018-08-20 17:07:51 +0200

    Aggiunta conferma tramite e-mail e reimpostazione pw dimenticata

--- FantaRuby/db/schema.rb
+++ FantaRuby/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_09_154901) do
+ActiveRecord::Schema.define(version: 2018_08_20_111255) do
 
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -120,6 +120,10 @@ ActiveRecord::Schema.define(version: 2018_08_09_154901) do
     t.string "twitter_user_secret"
     t.string "t_provider"
     t.string "t_uid"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
